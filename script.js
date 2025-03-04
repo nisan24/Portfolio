1c597b9b9d459c8c84472b88db469e9c9984ce74
Md. Nisan Hossain <nisanhossain76@gmail.com>
2025-03-04 19:54:30 +0600

    portfolio

---
+++ script.js
@@ -0,0 +1,22 @@
+// const toggleButton = document.getElementById('toggle-mode');
+
+// toggleButton.addEventListener('click', () => {
+//     document.body.classList.toggle('dark-mode');
+//     if (document.body.classList.contains('dark-mode')) {
+//         toggleButton.innerHTML = '🌞';
+//     } else {
+//         toggleButton.innerHTML = '🌙';
+//     }
+// });
+
+
+// Dark/Light mode toggle functionality
+// document.getElementById('toggle-mode').addEventListener('click', function () {
+//     document.body.classList.toggle('dark-mode');
+//     if (document.body.classList.contains('dark-mode')) {
+//         document.getElementById('toggle-mode').innerHTML = '🌞';
+//     } else {
+//         document.getElementById('toggle-mode').innerHTML = '🌙';
+//     }
+// });
+
